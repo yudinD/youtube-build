d9caef98c66bd805241de6421787c07c57994796
Dima <dima-mac@MacBook-Pro-Dima.local>
2020-07-18 18:21:51 -0500

    initial xcode project creation

---
+++ youtube-build/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-build
+//
+//  Created by Dima on 7/18/20.
+//  Copyright © 2020 Yudin Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
